753f3757478865922c231c52b2cb41be50438b92
“Djeutsch” <ldjeutchouang@aims.ac.tz>
2025-10-02 14:25:12 -0700

    Refactor regimes identifier and parall run code

--- step_4/submit_jobs.sh
+++ step_4/submit_jobs.sh
@@ -3,20 +3,17 @@
 # It uses a loop to iterate through different numbers of clusters.
 
 # Define variables for the script
-NUM_CLUSTs=(3 5 7 10 13 16 20 25) # Define the number of clusters (can be adjusted as needed)
+NUM_CLUSTS=(3 5 6 7 10 13 15 16 20 25) # Define the number of clusters (can be adjusted as needed)
 
-# Define the data domain and the scaling method
-DOMAIN="Global" # It can be adjusted as follows: "Global", NAReg", "SAReg", "CPReg" or "WGReg"
-SCALER="Standard" # It can be adjusted as follows: 'Quantile', 'Robust', 'Standard', 'Signed-Log', or 'Power-10'
 
 echo
-for nc in "${NUM_CLUSTs[@]}"
+for nc in "${NUM_CLUSTS[@]}"
 do
     # Submit the job to SLURM for each combination
     echo
-    echo "Running export NEMI clusters job for:"
-    echo " num_cluster=${nc}, region = ${DOMAIN}, data_scaler = ${SCALER}"
-    sbatch --job-name=NEMI:NC$nc run_nemi_clusters.sh "$nc" "$DOMAIN" "$SCALER"
+    echo "Running NEMI regime identification job for:"
+    echo " num_cluster=${nc}"
+    sbatch --job-name=NEMI:NC$nc run_identifier.sh "$nc"
 done
 
 echo
