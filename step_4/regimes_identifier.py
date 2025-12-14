cb380e1aeed9626e1325706aa6cdbc21a07db509
Laique Djeutchouang <Laique.Djeutchouang@noaa.gov>
2025-12-14 01:22:19 -0500

    Optimize across any clusters

--- step_4/regimes_identifier.py
+++ step_4/regimes_identifier.py
@@ -1,5 +1,6 @@
 # Export NEMI clusters
 
+import io
 import os
 import sys
 import time
@@ -7,8 +8,12 @@ import copy
 import numpy as np
 import xarray as xr
 
+# Ensure UTF-8 encoding for stdout
+if hasattr(sys.stdout, "buffer"):
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8") 
+
 # Add path to local modules and import them
-source = os.path.abspath('/home/djeutsch/Projects/seaLevelRegimes')
+source = os.path.abspath('/home/Laique.Djeutchouang/DEVs/BV-Regimes/NEMI/seaLevelRegimes')
 if source not in sys.path:
     sys.path.insert(1, source)
 
@@ -16,22 +21,20 @@ from src import nemi_func as nf # Importing the nemi_func module
 from src import aux_func as af # Importing the aux_func module
 
 
-
-
-
 # Define base directory for static files
-base_dir = '/group/maikesgrp/laique/NOAA/nemis'
-bvb_dir = '/group/maikesgrp/laique/NOAA/CM4X-BV-budgets'
+base_dir = '/work/lnd/CM4X/NEMI'
+bvb_dir = '/work/lnd/CM4X/BVB'
 
 def save_regimes_as_DataArray(regimes:list[np.ndarray], n_clusters:int,
-                              bvb_ds:xr.Dataset, output_dir: str):
+                              bvb_ds:xr.Dataset, bvb_geo_coords:xr.Dataset, output_dir: str):
     """
     Save NEMI regimes to NetCDF files.
 
     Args:
         regimes (list): List of identified clusters arrays.
         n_clusters (int): Number of clusters.
-        bvb_ds (xr.Dataset): Original BVD Dataset to reconstruct the clusters.
+        bvb_ds (xr.Dataset): Original BVB Dataset to reconstruct the clusters.
+        bvb_geo_coords (xr.Dataset): Dataset containing BVB correct geographical coordinates.
         output_dir (str): Directory to save the NetCDF output files.
     """
     
@@ -52,6 +55,10 @@ def save_regimes_as_DataArray(regimes:list[np.ndarray], n_clusters:int,
             clusters_ds = af.reconstruct_DataArray(bvb_ds=bvb_ds, embedded_nclusters_array=clusters)
             clusters_ds = clusters_ds.rename({'geo_cluster': 'regimes'})
             
+            # Assign correct geographical coordinates
+            clusters_ds = xr.merge([clusters_ds, bvb_geo_coords]) # Merge BVB dataset with geocoords dataset
+            clusters_ds = clusters_ds.set_coords(['geolat_c', 'geolon_c']) # Set geocoords as coordinates
+            
             # Save the DataArray to a NetCDF file
             clusters_ds.to_netcdf(output_filename)
             af.log_info(f"Regimes DataArray created and saved as: <{filename}>")
@@ -63,14 +70,14 @@ def save_regimes_as_DataArray(regimes:list[np.ndarray], n_clusters:int,
 
 
 
-def run_regimes_identifier(n_clusters:int, resolution:str, field:str, num_members:int=20):
+def run_regimes_identifier(data_res:str, data_field:str, n_clusters:int, num_members:int=20):
     """
     Main function to process, identify, and export geographical regimes.
     
     Args:
+        data_res (str): Resolution of the data.
+        data_field (str): 'statics' (mean) or 'dynamics' (e.g.; monthly climatology).
         n_clusters (int): Number of clusters.
-        resolution (str): Resolution of the data.
-        field (str): 'mean' for statics and anything else for dynamics.
         num_members (int, optional): Number of ensembles. Defaults to 20.
     """ 
     # List of UMAP key parameter combinations - minimum distances & nearest neighhbors.
@@ -87,14 +94,34 @@ def run_regimes_identifier(n_clusters:int, resolution:str, field:str, num_member
     print(f"{' '*5}• Cluster complexity level = {n_clusters}")
 
      # Declare entropy and clustering directories
-    if field == 'mean':
-        clust_dir = f'{base_dir}/CM4X-{resolution}/outputs/statics/clusterings'
-        entropy_dir = f'{base_dir}/CM4X-{resolution}/outputs/statics/entropy'
-        regimes_dir = f'{base_dir}/CM4X-{resolution}/outputs/statics/regimes'
+    if data_field == 'statics':
+        clust_dir = f'{base_dir}/CM4X-{data_res}/outputs/statics/clusterings'
+        entropy_dir = f'{base_dir}/CM4X-{data_res}/outputs/statics/entropy'
+        regimes_dir = f'{base_dir}/CM4X-{data_res}/outputs/statics/regimes'
+        
+        # Load static BVB data with geocoords and set geocoords as coordinates
+        bvb_path = f'{bvb_dir}/CM4X-{data_res}_BVB_and_tracers_geocoords_2005_2014_time_mean.zarr'
+        bvb_ds = xr.open_zarr(bvb_path, chunks=None)
+        
+        # Get correct geographical coordinates
+        bvb_geo_coords = bvb_ds[['geolat_c', 'geolon_c']].copy()
+        
+    elif data_field == 'dynamics':
+        clust_dir = f'{base_dir}/CM4X-{data_res}/outputs/dynamics/clusterings'
+        entropy_dir = f'{base_dir}/CM4X-{data_res}/outputs/dynamics/entropy'
+        regimes_dir = f'{base_dir}/CM4X-{data_res}/outputs/dynamics/regimes'
+        
+        # Load dynamic BVB data and static BVB data with geocoords
+        bvb_path = f'{bvb_dir}/CM4X-{data_res}_BVB_and_tracers_2005_2014_climatology.zarr'
+        bvb_ds = xr.open_zarr(bvb_path, chunks=None) # Load dynamic BVB data
+
+        # Get correct geographical coordinates. It must be extracted from static BVB data
+        bvb_path_ = f'{bvb_dir}/CM4X-{data_res}_BVB_and_tracers_geocoords_2005_2014_time_mean.zarr'
+        bvb_ = xr.open_zarr(bvb_path_, chunks=None) # Load static BVB data with geocoords
+        bvb_geo_coords = bvb_[['geolat_c', 'geolon_c']].copy() 
+        
     else:
-        clust_dir = f'{base_dir}/CM4X-{resolution}/outputs/dynamics/clusterings'
-        entropy_dir = f'{base_dir}/CM4X-{resolution}/outputs/dynamics/entropy'
-        regimes_dir = f'{base_dir}/CM4X-{resolution}/outputs/dynamics/regimes'
+        raise ValueError(f"Unknown field: {data_field}. Must be 'statics (mean)' or 'dynamics' (e.g.; monthly climatology).")   
     
     # Create output directory if it doesn't exist
     output_dir = f'{regimes_dir}/nclusters_{n_clusters}'
@@ -117,13 +144,9 @@ def run_regimes_identifier(n_clusters:int, resolution:str, field:str, num_member
     # Get NEMI clusters
     regimes = [nf.assess_overlap(ens_labels=labels[id, :, :], label_id=label_ids[id], num_members=num_members) for id in range(n_param)]
 
-    # Read the original Dataset file (netCDF)
-    bvb_path = f'{bvb_dir}/CM4X-{resolution}_BVB_2005_2014_time_mean.nc'
-    bvb_ds = xr.open_dataset(bvb_path)
-
     # Reconstruct NEMI clusters as a xarray DataArray and save
     save_regimes_as_DataArray(regimes=regimes, n_clusters=n_clusters,
-                              bvb_ds=bvb_ds, output_dir=output_dir)
+                              bvb_ds=bvb_ds, bvb_geo_coords=bvb_geo_coords, output_dir=output_dir)
     
     # Record the end time 
     end_now = nf.time_now()
@@ -141,19 +164,19 @@ if __name__ == "__main__":
     start_time = time.time()
     
     # Check if the correct number of command-line arguments are provided
-    if len(sys.argv) != 2:
-        print("Usage: python regimes_identifier.py <n_clusters>")
+    if len(sys.argv) != 4:
+        print("Usage: python regimes_identifier.py <data_res> <data_field> <n_clusters>")
         sys.exit(1)
     
     # Parse command-line arguments
-    n_clusters = int(sys.argv[1])
+    data_res = sys.argv[1]
+    data_field = sys.argv[2]
+    n_clusters = int(sys.argv[3])
     num_members = 20 # Number of ensemble members. Defaults to 20.
-    resolution = 'p125' 
-    field = 'mean'  # for 'statics' or 'dynamics', depending on your data
     
     # Call and run the regimes file identifier main function
-    run_regimes_identifier(n_clusters=n_clusters, resolution=resolution,
-                           field=field, num_members=num_members)
+    run_regimes_identifier(data_res=data_res, data_field=data_field,
+                           n_clusters=n_clusters, num_members=num_members)
     
     # Record the end time
     end_time = time.time()
